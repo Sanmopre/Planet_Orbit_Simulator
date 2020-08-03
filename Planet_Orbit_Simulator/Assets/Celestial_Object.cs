31779b3dfbdd7091a1bcaa797b96812575692c86
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-08-03 16:46:53 +0200

    Newtown gravity law basic

---
+++ Planet_Orbit_Simulator/Assets/Celestial_Object.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Celestial_Object : MonoBehaviour
+{
+    public Rigidbody Rb;
+    public Rigidbody Other_Celestial;
+    float G = 0.00000000000667f;
+    public float Mass_Multiplier = 100;
+
+    public Vector3 Initial_Force;
+    Vector3 Current_Force;
+
+
+    private void Start()
+    {
+        Rb.AddForce(Initial_Force);
+    }
+
+    public void Update()
+    {
+        Current_Force = Get_Force();
+        Rb.AddForce(Current_Force);
+    }
+
+
+    private Vector3 Get_Force() {
+        Vector3 force = Vector3.zero;
+        Vector3 unit_vec;
+
+        unit_vec = Other_Celestial.position - Rb.position;
+
+        float dst = Mathf.Sqrt( (Rb.position.x - Other_Celestial.position.x)* (Rb.position.x - Other_Celestial.position.x) + (Rb.position.y - Other_Celestial.position.y)* (Rb.position.y - Other_Celestial.position.y) + (Rb.position.z - Other_Celestial.position.z)* (Rb.position.z - Other_Celestial.position.z));
+
+        unit_vec = unit_vec / dst;
+
+        float top = G * Rb.mass * Other_Celestial.mass * Mass_Multiplier;
+
+        float magnitude = top / dst * dst;
+
+        force = magnitude * unit_vec;
+
+        return force;
+    }
+
+}
