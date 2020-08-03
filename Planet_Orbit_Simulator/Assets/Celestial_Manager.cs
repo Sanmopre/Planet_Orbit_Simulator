ed17ba3611c439778fc79097b32063374168ac65
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-08-03 20:17:58 +0200

    Now gets celestial objects from scene

--- Planet_Orbit_Simulator/Assets/Celestial_Manager.cs
+++ Planet_Orbit_Simulator/Assets/Celestial_Manager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Celestial_Manager : MonoBehaviour
 {
 
-    public Celestial_Object[] celestialObjects;
+    private Celestial_Object[] celestialObjects;
     public float Mass_Multiplier; 
 
     public void Update()
     {
+        celestialObjects = FindObjectsOfType<Celestial_Object>();
+
+        // Lookps for phy simlulation
         for (int i = 0; i < celestialObjects.Length; i++) {
             Vector3 Final_Force = Vector3.zero;
             for (int j = 0; j < celestialObjects.Length; j++) {
