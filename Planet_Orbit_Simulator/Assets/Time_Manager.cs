4e35f8f6c05369d1d97d7c8f44556cdfa6cf731b
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-08-04 13:03:23 +0200

    Added time scale component

---
+++ Planet_Orbit_Simulator/Assets/Time_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_Manager : MonoBehaviour
+{
+    public float slowdown_factor = 0.05f;
+
+    void Start()
+    {
+        Time.timeScale = slowdown_factor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+
+}
