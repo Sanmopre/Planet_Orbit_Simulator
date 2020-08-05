cb64fc32c267b3bba021d492476c15b57a0cb8a8
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-08-05 10:33:16 +0200

    camera state switch (kinda working :/)

--- Planet_Orbit_Simulator/Assets/Camera_Controller.cs
+++ Planet_Orbit_Simulator/Assets/Camera_Controller.cs
@@ -6,9 +6,14 @@ public class Camera_Controller : MonoBehaviour
 {
 
     public float RotationSpeed = 1;
-    public Transform Target, Player;
+    public Transform Target;
+
+    public Transform Camera;
+
+    private GameObject Cel_Obj;
     float mouseX, mouseY;
 
+    public bool focused_on_planet = false;
 
 
     void Start()
@@ -23,7 +28,7 @@ public class Camera_Controller : MonoBehaviour
         mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
         mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
 
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
+        mouseY = Mathf.Clamp(mouseY, -45, 70);
 
         transform.LookAt(Target);
 
@@ -33,8 +38,44 @@ public class Camera_Controller : MonoBehaviour
     }
 
 
-    void LateUpdate()
+    public void FixCameraPlanet(Transform newParent)
     {
+        // Sets "newParent" as the new parent of the child GameObject.
+        Target.transform.SetParent(newParent);
+        Vector3 Cel_Position = newParent.transform.position;
+
+        Target.transform.position = Vector3.zero + Cel_Position;
+        Target.transform.rotation = Quaternion.identity;
+        Target.transform.localScale = Vector3.one;
+
+        Camera.transform.position = new Vector3(0.0f, 0.0f, -350.0f);
+        focused_on_planet = true;
+    }
+
+
+    public void UnfixCameraPlanet() {
+        Target.transform.SetParent(null);
+        Target.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        Camera.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        focused_on_planet = false;
+
+    }
+    
+
+    void LateUpdate()
+    {        
+        
         CamControl();
+        if (Input.GetKeyDown("space") && focused_on_planet == false)
+        {
+            Cel_Obj = GameObject.Find("SUN");
+            FixCameraPlanet(Cel_Obj.transform);
+        }
+        else if (Input.GetKeyDown("space") && focused_on_planet == true) {
+            UnfixCameraPlanet();
+        }
+
+
+
     }
 }
